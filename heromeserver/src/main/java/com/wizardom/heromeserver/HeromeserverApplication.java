ab7fb15004626781d85c98468b8c18d3309aeacd
Ibekason Alexander <alibekason@gmail.com>
2021-05-04 16:42:57 +0100

    Bootstrap server for logging and app metrics

---
+++ heromeserver/src/main/java/com/wizardom/heromeserver/HeromeserverApplication.java
@@ -0,0 +1,13 @@
+package com.wizardom.heromeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeromeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeromeserverApplication.class, args);
+    }
+
+}
---
+++ heromeserver/src/test/java/com/wizardom/heromeserver/HeromeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizardom.heromeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeromeserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
