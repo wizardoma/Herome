4b409979ee486f188f190adb4cdcec1cd8dc942a
Ibekason Alexander <alibekason@gmail.com>
2021-05-13 20:32:00 +0100

    Add authentication states

--- heromeapp/lib/application/authentication/auth_state.dart
+++ heromeapp/lib/application/authentication/auth_state.dart
@@ -1,1 +1,9 @@
 abstract class AuthenticationState {}
+
+class Authenticating extends AuthenticationState{}
+class Authenticated extends AuthenticationState {}
+class AuthenticationError extends AuthenticationState {
+  final String errorMessage;
+
+  AuthenticationError(this.errorMessage);
+}
