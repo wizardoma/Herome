d4b45639cc24ca1f3e9882e705dfb64a86891e03
Ibekason Alexander <alibekason@gmail.com>
2021-05-13 18:11:05 +0100

    Enhance app styles and colors

--- heromeapp/lib/commons/colors.dart
+++ heromeapp/lib/commons/colors.dart
@@ -1,9 +1,14 @@
 import 'dart:ui';
- const kPrimaryColor = const Color(0xff79589F);
- const kTextColor = const Color(0xffCFD7E6);
- const kHeaderColor = const Color(0xff9E7CC1);
- const kContainerBackgroundColor = const Color(0xffe7ebf3);
- const kDarkTextColor = const Color(0xff596981);
- const kGreyTextColor = const Color(0xff62738d);
- const kInputBorderColor = const Color(0xffcfd7e6);
- const kDarkGrey = const Color(0xff475366);
+
+const kPrimaryColor = const Color(0xff79589F);
+const kDeepPurple1 = const Color(0xff654a86);
+const kWhiteColor = const Color(0xffffffff);
+const kFooterBackgroundColor = const Color(0xfff5f5f5);
+const kDeepPurple2 = const Color(0xff534292);
+const kTextColor = const Color(0xffCFD7E6);
+const kHeaderColor = const Color(0xff9E7CC1);
+const kContainerBackgroundColor = const Color(0xffe7ebf3);
+const kDarkTextColor = const Color(0xff596981);
+const kGreyTextColor = const Color(0xff62738d);
+const kInputBorderColor = const Color(0xffcfd7e6);
+const kDarkGrey = const Color(0xff475366);
--- heromeapp/lib/commons/themes.dart
+++ heromeapp/lib/commons/themes.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:heromeapp/commons/colors.dart';
 
 var kMainTheme = ThemeData(
-  primaryColor: kPrimaryColor,
-  accentColor: kGreyTextColor
-);
+    primaryColor: kPrimaryColor,
+    accentColor: kGreyTextColor,
+    textTheme: TextTheme(
+        bodyText1: TextStyle(
+          color: kDarkTextColor,
+        ),
+        headline1: TextStyle(
+          color: kPrimaryColor,
+        )));
