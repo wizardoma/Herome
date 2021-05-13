95bcf3bfed9105ad5fb49337f7ba90a0a02d2282
Ibekason Alexander <alibekason@gmail.com>
2021-05-13 20:33:14 +0100

    inject auth service to  the state manager

--- heromeapp/lib/application/authentication/auth_bloc.dart
+++ heromeapp/lib/application/authentication/auth_bloc.dart
@@ -1,14 +1,30 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:heromeapp/application/authentication/auth_event.dart';
 import 'package:heromeapp/application/authentication/auth_state.dart';
+import 'package:heromeapp/application/authentication/login_request.dart';
+import 'package:heromeapp/domain/auth/auth_service.dart';
 
 class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc(AuthenticationState initialState) : super(initialState);
+  final AuthenticationService authenticationService;
+  AuthenticationBloc(this.authenticationService) : super(Authenticating());
 
   @override
-  Stream<AuthenticationState> mapEventToState(AuthenticationEvent event) {
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+  Stream<AuthenticationState> mapEventToState(AuthenticationEvent event) async*{
+    if (event is LoginEvent) {
+      yield await login(event.loginRequest);
+    }
   }
 
+  Future<AuthenticationState> login(LoginRequest loginRequest)async {
+    var response =await authenticationService.authenticate(loginRequest);
+    if (!response.isError){
+      return Authenticated();
+    }
+
+    else {
+      return AuthenticationError(response.errors.message);
+
+    }
+
+  }
 }
