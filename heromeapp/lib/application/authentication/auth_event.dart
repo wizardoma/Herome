a1121c9d3e079fe13b1f8c8c746d8914d0351d93
Ibekason Alexander <alibekason@gmail.com>
2021-05-13 20:31:25 +0100

    Add login event

--- heromeapp/lib/application/authentication/auth_event.dart
+++ heromeapp/lib/application/authentication/auth_event.dart
@@ -1,1 +1,9 @@
+import 'package:heromeapp/application/authentication/login_request.dart';
+
 abstract class AuthenticationEvent {}
+
+class LoginEvent extends AuthenticationEvent {
+  final LoginRequest loginRequest;
+
+  LoginEvent(this.loginRequest);
+}
