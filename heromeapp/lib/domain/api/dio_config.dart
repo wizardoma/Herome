415253bb3aea51eb28f3fe42e9bdd1a91e7858b5
Ibekason Alexander <alibekason@gmail.com>
2021-05-14 13:37:43 +0100

    Set configuration for http client requests

---
+++ heromeapp/lib/domain/api/dio_config.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:heromeapp/domain/api/heroku_api.dart';
+import 'package:heromeapp/domain/auth/auth_store.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Dio dio = Dio(BaseOptions(
+  baseUrl: baseUrl,
+))
+  ..interceptors.add(InterceptorsWrapper(
+    onRequest: (options, handler) => requestInterceptors(options, handler),
+  ));
+
+requestInterceptors(RequestOptions options, RequestInterceptorHandler handler) async {
+  options.headers
+      .putIfAbsent("Accept", () => "application/vnd.heroku+json; version=3");
+
+  var pref = await SharedPreferences.getInstance();
+  var token = pref.getString(AuthStore.prefName);
+  options.headers.putIfAbsent("Authorization", () => "Basic $token");
+  print("Request data ${options.headers}, token : $token");
+  handler.next(options);
+}
---
+++ heromeapp/lib/domain/api/heroku_api.dart
@@ -0,0 +1,3 @@
+final String baseUrl = "https://api.heroku.com";
+
+final String accountUrl = "$baseUrl/account";
