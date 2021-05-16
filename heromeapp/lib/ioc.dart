7532a3550ffbb92553946391705f0fe1eedc8800
Ibekason Alexander <alibekason@gmail.com>
2021-05-16 11:32:39 +0100

    update dyno to IoC

--- heromeapp/lib/ioc.dart
+++ heromeapp/lib/ioc.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:heromeapp/application/accounts/account_cubit.dart';
 import 'package:heromeapp/application/apps/apps_cubit.dart';
 import 'package:heromeapp/application/authentication/auth_bloc.dart';
+import 'package:heromeapp/application/dyno/dyno_cubit.dart';
 import 'package:heromeapp/domain/account/account_provider.dart';
 import 'package:heromeapp/domain/account/account_service.dart';
 import 'package:heromeapp/domain/account/account_service_impl.dart';
@@ -14,11 +15,14 @@ import 'package:heromeapp/domain/auth/auth_provider.dart';
 import 'package:heromeapp/domain/auth/auth_service.dart';
 import 'package:heromeapp/domain/auth/auth_service_impl.dart';
 import 'package:heromeapp/domain/auth/auth_store.dart';
+import 'package:heromeapp/domain/dyno/dyno_provider.dart';
+import 'package:heromeapp/domain/dyno/dyno_service.dart';
+import 'package:heromeapp/domain/dyno/dyno_service_impl.dart';
 import 'package:heromeapp/domain/service.dart';
 
 // Custom inversion of control container
 enum Cubits {
-  Account, Apps
+  Account, Apps, Dyno
 }
 
 enum Blocs {
@@ -26,18 +30,20 @@ enum Blocs {
 }
 
 enum Services {
-  Account, Authentication, Apps
+  Account, Authentication, Apps, Dyno
 }
 
 
 class IOC {
   //services
   AuthenticationService _authenticationService;
+  DynoService _dynoService;
   AccountService _accountService;
   AppService _appService;
 
   // providers
   AuthProvider _authProvider;
+  DynoProvider _dynoProvider;
   AccountProvider _accountProvider;
   AppProvider _appProvider;
 
@@ -47,6 +53,7 @@ class IOC {
   // blocs and cubit
   AuthenticationBloc _authenticationBloc;
   AccountCubit _accountCubit;
+  DynoCubit _dynoCubit;
   AppsCubit _appsCubit;
 
   Map<Blocs, Bloc> blocMap = {};
@@ -66,26 +73,35 @@ class IOC {
     // providers
 
     _authProvider = AuthProvider(dio);
+    _dynoProvider = DynoProvider(dio);
     _accountProvider = AccountProvider(dio);
     _appProvider = AppProvider(dio);
 
     // services
     _authenticationService = HerokuAuthenticationService(_authProvider, _authStore);
     _accountService = HerokuAccountService(_accountProvider);
+    _dynoService = DynoServiceImpl(_dynoProvider);
     _appService = AppServiceImpl(_appProvider);
 
     //blocs
     _authenticationBloc = AuthenticationBloc(_authenticationService);
     _accountCubit = AccountCubit(authenticationBloc: _authenticationBloc,accountService: _accountService);
 _appsCubit = AppsCubit(_appService);
+    _dynoCubit = DynoCubit(dynoService: _dynoService, appsCubit: _appsCubit);
+
 
     blocMap.putIfAbsent(Blocs.Authentication, () => _authenticationBloc);
     cubitMap.putIfAbsent(Cubits.Account, () => _accountCubit);
     cubitMap.putIfAbsent(Cubits.Apps, () => _appsCubit);
+    cubitMap.putIfAbsent(Cubits.Dyno, () => _dynoCubit);
+
 
     serviceMap.putIfAbsent(Services.Account, () => _accountService);
+
     serviceMap.putIfAbsent(Services.Authentication, () => _authenticationService);
     serviceMap.putIfAbsent(Services.Apps, () => _appService);
+    serviceMap.putIfAbsent(Services.Dyno, () => _dynoService);
+
 
   }
 
--- heromeapp/lib/main.dart
+++ heromeapp/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:heromeapp/application/accounts/account_cubit.dart';
 import 'package:heromeapp/application/apps/apps_cubit.dart';
 import 'package:heromeapp/application/authentication/auth_bloc.dart';
 import 'package:heromeapp/application/authentication/auth_event.dart';
+import 'package:heromeapp/application/dyno/dyno_cubit.dart';
 import 'package:heromeapp/commons/app/routes.dart';
 import 'package:heromeapp/commons/app/themes.dart';
 import 'package:heromeapp/ioc.dart';
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         BlocProvider.value(
             value: (ioc.getCubit(Cubits.Account) as AccountCubit)),
         BlocProvider.value(value: (ioc.getCubit(Cubits.Apps) as AppsCubit)),
+        BlocProvider.value(value: (ioc.getCubit(Cubits.Dyno) as DynoCubit)),
       ],
       child: MaterialApp(
         routes: appRoutes,
