bb4a511547f4a52b13221ee0144c59edd349d4a1
Ibekason Alexander <alibekason@gmail.com>
2021-05-20 16:26:45 +0100

    add ui to display collaborators of an app

--- heromeapp/lib/presentation/app_access/app_access_screen.dart
+++ heromeapp/lib/presentation/app_access/app_access_screen.dart
@@ -1,22 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:heromeapp/application/access/collaborator_cubit.dart';
+import 'package:heromeapp/application/access/collaborator_state.dart';
 import 'package:heromeapp/domain/apps/app.dart';
 import 'package:heromeapp/presentation/widgets/app_bottomnav_items_scaffolds.dart';
-import 'package:heromeapp/presentation/widgets/appscreen_appbar.dart';
+import 'package:heromeapp/presentation/widgets/circular_progress_primary.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppAccessScreen extends StatelessWidget {
+class AppAccessScreen extends StatefulWidget {
   final App app;
-AppAccessScreen(this.app);
 
+  AppAccessScreen(this.app);
 
-@override
-  Widget build(BuildContext context) {
-  return AppItemsScaffold(body: Center(
-    child: Text("Access"),
-  ), appBarTitle: 'Collaborators',
-
-  );
-
-
-
+  @override
+  _AppAccessScreenState createState() => _AppAccessScreenState();
 }
+
+class _AppAccessScreenState extends State<AppAccessScreen> {
+  RefreshController _refreshController;
+  CollaboratorCubit collaboratorCubit;
+
+  void onRefresh() async {
+    await collaboratorCubit.fetchCollaborators(widget.app.id);
+    _refreshController.refreshCompleted();
+  }
+
+  @override
+  void initState() {
+    collaboratorCubit = context.read<CollaboratorCubit>();
+    print("init access ${collaboratorCubit.collabs}");
+    var hasFetched = collaboratorCubit.collabs.length > 0;
+    _refreshController = RefreshController(initialRefresh: false);
+
+    if (!hasFetched) {
+      collaboratorCubit.fetchCollaborators(widget.app.id);
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _refreshController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppItemsScaffold(
+      onRefresh: onRefresh,
+      body: BlocBuilder<CollaboratorCubit, CollaboratorState>(
+        // ignore: missing_return
+        builder: (BuildContext context, state) {
+          if (state is CollaboratorFetchingState) {
+            return Center(child: CircularProgress());
+          }
+          if (state is CollaboratorFetchError) {
+            return Center(
+              child: Text(state.error),
+            );
+          }
+          if (state is CollaboratorFetchedState) {
+            var collabs = state.collabs;
+            return SmartRefresher(
+              controller: _refreshController,
+              onRefresh: onRefresh,
+              enablePullDown: true,
+              child: ListView.builder(
+                  itemCount: collabs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Icon(Icons.person, size: 30,),
+                      title: Text(collabs[index].userEmail),
+                      subtitle: Text(collabs[index].role),
+                      trailing: PopupMenuButton(
+                        icon: Icon(Icons.more_vert, color: Colors.black87),
+                        itemBuilder: (context) => [
+                          PopupMenuItem(child: Text("Change Role"), value: "role",),
+                          PopupMenuItem(child: Text("Remove"), value: "remove",),
+                        ],
+                      ),
+                    );
+                  }),
+            );
+          }
+        },
+      ),
+      appBarTitle: 'Collaborators',
+    );
+  }
 }
